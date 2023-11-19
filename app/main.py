7652573e0beacd1ea4a61831d2390ae5d68c21f7
F0rtis <dedanik12@gmail.com>
2023-11-19 22:15:18 +0300

    initial fast api

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+from fastapi import FastAPI, Response, status
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return Response(status_code=status.HTTP_200_OK, content="OK")
